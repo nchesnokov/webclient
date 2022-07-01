03e279fedca2f8c06dd38a7f2acfe39bec6d2ab9
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2022-07-01 15:20:13 +0300

    Go to proxy components

--- src/components/static/gp-mdx.vue
+++ src/components/static/gp-mdx.vue
@@ -1,128 +1,180 @@
 <template>
   <div>
-    <DxChart ref="chart">
-      <DxTooltip
-        :enabled="true"
-        :customize-tooltip="customizeTooltip"
-      />
-      <DxAdaptiveLayout :width="450"/>
-      <DxSize :height="200"/>
-      <DxCommonSeriesSettings type="bar"/>
-    </DxChart>
-
-    <DxPivotGrid
-      id="pivotgrid"
-      ref="grid"
-      :data-source="dataSource"
-      :allow-sorting-by-summary="true"
-      :allow-filtering="true"
-      :show-borders="true"
-      :show-column-grand-totals="false"
-      :show-row-grand-totals="false"
-      :show-row-totals="false"
-      :show-column-totals="false"
+    <v-layout row>
+      <v-flex xs1 sm1 md1 lg1 xl1>
+        <v-btn icon color="purple" @click="on_configure_view(model)"
+          ><v-icon>settings</v-icon></v-btn
+        >
+      </v-flex>
+      <v-flex xs2 sm2 md2 lg2 xl2>
+        <v-select label="Criteria" :items="rfields" v-model="rfield" />
+      </v-flex>
+      <v-flex xs1 sm1 md1 lg1 xl1> </v-flex>
+      <v-flex xs2 sm2 md2 lg2 xl2>
+        <el-select label="Aggregate" :items="aggregates" v-model="aggregate">
+		          <el-option
+            v-for="item in aggregates"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+		</el-select>
+      </v-flex>
+    </el-layout>
+    <pivot
+      :data="data"
+      :fields="fields"
+      :row-fields="rowFields"
+      :col-fields="colFields"
+      :reducer="reducer"
+      ref="pivot"
     >
-      <DxFieldChooser
-        :enabled="true"
-        :height="400"
-      />
-    </DxPivotGrid>
+      <!-- Optional slots can be used for formatting, see documentation below -->
+    </pivot>
   </div>
 </template>
+
+
+
+
 <script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "gsrp5-mdx-view",
+});
+</script>
+
+<script setup>
+
+import Pivot from '@marketconnect/vue-pivot-table';
+import PivotTable from '@marketconnect/vue-pivot-table';
 
 import {
-  DxChart,
-  DxAdaptiveLayout,
-  DxCommonSeriesSettings,
-  DxSize,
-  DxTooltip,
-} from 'devextreme-vue/chart';
+onBeforeMount,
+  ref,
+  reactive,
+  watch,
+  getCurrentInstance
+} from "vue";
 
-import {
-  DxPivotGrid,
-  DxFieldChooser,
-} from 'devextreme-vue/pivot-grid';
-
-import { sales } from './data.js';
-
-const currencyFormatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 0,
+const props = defineProps({
+  cid: {
+    type: String,
+    required: true,
+  },
+  metas: {
+    type: Object,
+  },
+  model: {
+    type: String,
+    required: true,
+  }
 });
 
-export default {
-  components: {
-    DxChart,
-    DxAdaptiveLayout,
-    DxCommonSeriesSettings,
-    DxSize,
-    DxTooltip,
-    DxPivotGrid,
-    DxFieldChooser,
-  },
-  data() {
-    return {
-      dataSource: {
-        fields: [{
-          caption: 'Region',
-          width: 120,
-          dataField: 'region',
-          area: 'row',
-          sortBySummaryField: 'Total',
-        }, {
-          caption: 'City',
-          dataField: 'city',
-          width: 150,
-          area: 'row',
-        }, {
-          dataField: 'date',
-          dataType: 'date',
-          area: 'column',
-        }, {
-          groupName: 'date',
-          groupInterval: 'month',
-          visible: false,
-        }, {
-          caption: 'Total',
-          dataField: 'amount',
-          dataType: 'number',
-          summaryType: 'sum',
-          format: 'currency',
-          area: 'data',
-        }],
-        store: sales,
-      },
-      customizeTooltip(args) {
-        const valueText = currencyFormatter.format(args.originalValue);
-        return {
-          html: `${args.seriesName} | Total<div class='currency'>${valueText}</div>`,
-        };
-      },
-    };
-  },
-  mounted() {
-    const pivotGrid = this.$refs.grid.instance;
-    const chart = this.$refs.chart.instance;
-    pivotGrid.bindChart(chart, {
-      dataFieldsDisplayMode: 'splitPanes',
-      alternateDataFields: false,
-    });
-    const dataSource = pivotGrid.getDataSource();
-    setTimeout(() => {
-      dataSource.expandHeaderItem('row', ['North America']);
-      dataSource.expandHeaderItem('column', [2013]);
-    }, 0);
-  },
-};
-</script>
-<style>
-#pivotgrid {
-  margin-top: 20px;
-}
 
-.currency {
-  text-align: center;
-}
-</style>
+     const { proxy } = getCurrentInstance(); 
+	  const data = reactive([]);
+      const fields = reactive([]);
+      const colFields = reactive([]);
+      const rowFields = reactive([]);
+      const reducer = ref((sum, item) =>{
+        return sum + 1;
+      });
+      const selectionFrom = reactive({});
+      const selectionFromForm = reactive({});
+      const vfields = reactive([]);
+      const rfields = reactive([]);
+      const rfield = ref(null);
+      const aggregates = reactive([{text:'Count',value:'count'},{text:'Sum',value:'sum'},{text:'Average',value:'avg'},{text:'Min',value:'min'},{text:'Max',value:'max'}]);
+      const aggregate =  ref('sum');
+      const template = ref(null);
+    watch( aggregate,(aggregate,prevAggregate) => {
+		switch(aggregate){
+			case 'count':
+				reducer.value = new Function('count','item', 'return count + 1');
+				break;
+			case 'sum':
+				reducer.value = new Function('sum','item', 'return sum + item.'+n);
+				break;
+			case 'avg':
+				reducer.value = new Function('avg','item', 'return (avg + item.'+n+')/2');
+				break;
+			case 'min':
+				reducer.value = new Function('min','item', 'return min < item ? min : item.'+n);
+				break;
+			case 'max':
+				reducer.value = new Function('max','item', 'return max > item ? max : item.'+n);
+				break;	
+			}
+		});
+		
+    const onMDX = (rows)=>{
+			if (rows.length > 0) {
+        data.splice(0,data.length,...rows)
+				}
+			};	
+	onBeforeMount(async () => {
+		// this.meta = this.metas[this.model].meta;
+		// this.views = this.metas[this.model].views;
+		// this.view = this.views.mdx;
+		// this.name = this.meta.description;
+		// this.actions = this.meta.actions;
+			let a0 = [],selections = [];
+
+			// this.view_variants = this.view.confs.map(function(a){return a.name;});
+			// this.selected_view_variant = this.view_variants[0];
+			// this.show_cols = this.view.confs[0].values.map(function(s){return s.col});
+			// var show_cols = this.show_cols;
+			// if (this.show_cols.length > 0) this.vfields = Object.keys(this.view.columns).filter(function(c){return show_cols.indexOf(c) >=0;});
+			// else this.vfields = Object.keys(this.view.columns);
+			vfields.splice(0,vfields.length, ...props.metas[props.model].views.mdx.columns.map((v) => v.col))
+			
+			for(let i = 0; i < vfields.length; i++) {
+				switch(props.metas[props.model].meta.columns[vfields[i]].type){
+					case 'char':
+					case 'varchar':
+						if (vfields[i] == props.metas[props.model].meta.names.rec_name) colFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter': new Function('item','return item.'+vfields[i])});
+						else rowFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter': new Function('item','return item.'+vfields[i])});
+						break;					
+					case 'date':
+					case 'time':
+					case 'datatime':
+					case 'text':
+					case 'xml':
+						rowFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter': new Function('item','return item.'+vfields[i])});
+						break;
+					case 'many2one':
+					case 'related':
+						rowFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter':new Function('item','return item.'+vfields[i]+'.name')});
+						break;
+					case 'selection':
+						selectionFromForm[vfields[i]] = {};
+						selections = props.metas[props.model].meta.columns[vfields[i]].selections;
+						for(let j = 0; j < selections.length; j++){
+							a0.push({text:selections[j][1],value:selections[j][0]});
+							selectionFromForm[vfields[i]][selections[j][0]] = selections[j][1];
+							}
+						selectionFrom[vfields[i]] = a0;
+						rowFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter': new Function('item','return item.'+vfields[i])});
+						break;
+					case 'integer':
+					case 'float':
+					case 'double':
+					case 'real':
+					case 'decimal':
+					case 'numeric':
+						rfields.push(vfields[i]);
+						break;
+					default:
+						rowFields.push({'label':props.metas[props.model].meta.columns[vfields[i]].label,'getter': new Function('item','return item.' + vfields[i])});
+				}
+			} 
+			rfield.value = rfields[0];
+			reducer.value = new Function('sum','item', 'return sum + item.' + rfield);
+
+		onMDX( await proxy.$ws.sendAsynvc({_msg:[props.cid,'models',props.model,'select',{fields: vfields, context: proxy.$UserPreferences.Context}]}));
+		});
+
+</script>
+
--- src/components/static/gp-schedule.vue
+++ src/components/static/gp-schedule.vue
@@ -1,82 +1,34 @@
-<template>
-  <DxScheduler
-    time-zone="America/Los_Angeles"
-    :data-source="dataSource"
-    :current-date="currentDate"
-    :views="views"
-    :groups="groups"
-    :height="600"
-    :show-all-day-panel="true"
-    :first-day-of-week="1"
-    :start-day-hour="8"
-    :end-day-hour="18"
-    current-view="month"
-    data-cell-template="dataCellTemplate"
-    resource-cell-template="resourceCellTemplate"
-  >
-    <DxResource
-      :data-source="employees"
-      :allow-multiple="false"
-      label="Employee"
-      field-expr="employeeID"
-    />
-
-    <template #resourceCellTemplate="{ data: employee }">
-      <ResourceCell
-        :employee="employee"
-      />
-    </template>
-
-    <template #dataCellTemplate="{ data: cellData }">
-      <DataCell
-        :cell-data="cellData"
-      />
-    </template>
-  </DxScheduler>
-</template>
 <script>
-
-import { DxScheduler, DxResource } from 'devextreme-vue/scheduler';
-
-import { employees, data } from './data2.js';
-
-import DataCell from './DataCell.vue';
-import ResourceCell from './ResourceCell.vue';
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
   components: {
-    DxScheduler,
-    DxResource,
-    DataCell,
-    ResourceCell,
+    FullCalendar // make the <FullCalendar> tag available
   },
   data() {
     return {
-      groups: ['employeeID'],
-      views: ['month'],
-      currentDate: new Date(2021, 5, 2, 11, 30),
-      employees,
-      dataSource: data,
-    };
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        weekends: false,
+                events: [
+          { title: 'event 1', date: '2022-07-01' },
+          { title: 'event 2', date: '2022-07-04' }
+        ] // initial value
+      }
+    }
   },
-};
+  methods: {
+    toggleWeekends: function() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    }
+  }
+}
 </script>
-<style>
-.dx-scheduler-group-header,
-.dx-scheduler-date-table-cell {
-  position: relative;
-}
-
-.dx-scheduler-date-table-other-month.dx-scheduler-date-table-cell {
-  opacity: 1;
-  color: rgba(0, 0, 0, 0.3);
-}
-
-.dx-scheduler-date-table-cell .dx-template-wrapper {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding-right: 6px;
-}
-
-</style>
+<template>
+  <button @click="toggleWeekends">toggle weekends</button>
+  <FullCalendar :options="calendarOptions" />
+</template>
